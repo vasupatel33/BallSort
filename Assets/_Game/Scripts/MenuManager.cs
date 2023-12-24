44a16fe2e952891c35692059e32b4ae9004a99f5
vasupatel <vasurabadiya5@gmail.com>
2023-12-24 23:01:04 +0530

    Music added

--- Assets/_Game/Scripts/MenuManager.cs
+++ Assets/_Game/Scripts/MenuManager.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] GameObject SettingPanel;
-
+    [SerializeField] Button MusicBtn, SoundBtn;
+    [SerializeField] Sprite MusicOnImg, SoundOnImg, MusicOffImg, SoundOffImg;
+    [SerializeField] AudioClip ClickSound;
     public void OnSettingPanelOpen()
     {
         SettingPanel.SetActive(true);
@@ -22,5 +25,64 @@ public class MenuManager : MonoBehaviour
     public void OnApplicationQuit()
     {
         Application.Quit();
+    }
+    public void MusicManager()
+    {
+        if (Common.Instance.isMusicPlaying == false)
+        {
+            Common.Instance.gameObject.transform.GetChild(0).GetComponent<AudioSource>().mute = false;
+            MusicBtn.GetComponent<Image>().sprite = MusicOnImg;
+            Common.Instance.isMusicPlaying = true;
+        }
+        else
+        {
+            Common.Instance.gameObject.transform.GetChild(0).GetComponent<AudioSource>().mute = true;
+            MusicBtn.GetComponent<Image>().sprite = MusicOffImg;
+            Common.Instance.isMusicPlaying = false;
+        }
+    }
+    public void MusicSet()
+    {
+        if (Common.Instance.isMusicPlaying == false)
+        {
+            Common.Instance.gameObject.transform.GetChild(0).GetComponent<AudioSource>().mute = true;
+            MusicBtn.GetComponent<Image>().sprite = MusicOffImg;
+        }
+        else
+        {
+            Common.Instance.gameObject.transform.GetChild(0).GetComponent<AudioSource>().mute = false;
+            MusicBtn.GetComponent<Image>().sprite = MusicOnImg;
+
+        }
+    }
+    public void SoundManager()
+    {
+        //Common.Instance.gameObject.transform.GetChild(1).GetComponent<AudioSource>().PlayOneShot(ClickSound);
+        if (Common.Instance.isSoundPlaying == false)
+        {
+            Common.Instance.gameObject.transform.GetChild(1).GetComponent<AudioSource>().mute = false;
+            SoundBtn.GetComponent<Image>().sprite = SoundOnImg;
+            Common.Instance.isSoundPlaying = true;
+        }
+        else
+        {
+            Common.Instance.gameObject.transform.GetChild(1).GetComponent<AudioSource>().mute = true;
+            SoundBtn.GetComponent<Image>().sprite = SoundOffImg;
+            Common.Instance.isSoundPlaying = false;
+        }
+    }
+    public void SoundSet()
+    {
+        if (Common.Instance.isSoundPlaying == false)
+        {
+            Common.Instance.gameObject.transform.GetChild(1).GetComponent<AudioSource>().mute = true;
+            SoundBtn.GetComponent<Image>().sprite = SoundOffImg;
+        }
+        else
+        {
+            Common.Instance.gameObject.transform.GetChild(1).GetComponent<AudioSource>().mute = false;
+            SoundBtn.GetComponent<Image>().sprite = SoundOnImg;
+
+        }
     }
 }
